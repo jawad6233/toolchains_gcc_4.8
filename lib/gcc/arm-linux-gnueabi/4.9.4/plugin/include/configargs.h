2d8e06d9d7cc9f3c0417d2b16d2ea3e6e9211497
mayankbali <mayankbali3@gmail.com>
2017-11-10 08:38:08 +0000

    gcc-4.8

---
+++ lib/gcc/arm-linux-gnueabi/4.9.4/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror --enable-languages=c --target=arm-linux-gnueabi --prefix=/home/mayankbali3/build-tools-gcc/arm-linux-gnueabi";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "tls", "gnu" } };
